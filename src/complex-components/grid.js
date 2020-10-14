36eaf77617f760d719864109339927e44a6daf29
Nikolay Dzhurov <chinzo.nd@gmail.com>
2020-10-14 22:30:08 +0300

    Use functional components instead of class components

--- src/complex-components/grid.js
+++ src/complex-components/grid.js
@@ -2,23 +2,17 @@ import React from 'react';
 
 import Row from 'simple-components/row';
 
-export default class Grid extends React.Component {
-	render() {
-		const {rowData, className, onCellClick} = this.props;
-
-		return (
-			<table className={className}>
-				<tbody>
-					{rowData.map((data, idx) => (
-						<Row
-							key={idx}
-							rowIndex={idx}
-							onCellClick={onCellClick}
-							{...data}
-						/>
-					))}
-				</tbody>
-			</table>
-		);
-	}
-}
+export default ({ rowData, className, onCellClick }) => (
+	<table className={className}>
+		<tbody>
+			{rowData.map((data, idx) => (
+				<Row
+					key={idx}
+					rowIndex={idx}
+					onCellClick={onCellClick}
+					{...data}
+				/>
+			))}
+		</tbody>
+	</table>
+);
--- src/simple-components/cell.js
+++ src/simple-components/cell.js
@@ -1,14 +1,13 @@
 import React from 'react';
 
-export default class Cell extends React.PureComponent {
-	render() {
-		const {rowIndex, cellIndex, value, className, onClick} = this.props;
-		const clickHandler = onClick ? () => onClick(rowIndex, cellIndex) : undefined;
-
-		return (
-			<td className={className || undefined} onClick={clickHandler}>
-				{value}
-			</td>
-		);
-	}
-}
+export default ({
+	rowIndex,
+	cellIndex,
+	value,
+	className,
+	onClick,
+}) => (
+	<td className={className || undefined} onClick={onClick ? () => onClick(rowIndex, cellIndex) : undefined}>
+		{value}
+	</td>
+);
