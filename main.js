832150a96a0580d72ed86318e70d26ff88bf3836
Nikolay Dzhurov <chinzo.nd@gmail.com>
2020-10-14 22:00:23 +0300

    Add electron application with build configurations

---
+++ main.js
@@ -0,0 +1,44 @@
+const { app, BrowserWindow, Menu } = require('electron');
+
+const isMac = process.platform === 'darwin';
+
+function createWindow() {
+	const win = new BrowserWindow({
+		width: 600,
+		height: 800,
+		webPreferences: {
+			nodeIntegration: true,
+		},
+	});
+
+	win.openDevTools();
+	win.loadFile('./build/index.html');
+	win.once('ready-to-show', () => {
+		win.show();
+		win.focus();
+	});
+
+	const menu = new Menu();
+	menu.setApplicationMenu(
+		menu.buildFromTemplate([{
+			label: 'File',
+			submenu: [
+				isMac ? { role: 'close' } : { role: 'quit' },
+			],
+		}]),
+	);
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+	if (isMac) {
+		app.quit();
+	}
+});
+
+app.on('activate', () => {
+	if (BrowserWindow.getAllWindows().length === 0) {
+		createWindow();
+	}
+});
