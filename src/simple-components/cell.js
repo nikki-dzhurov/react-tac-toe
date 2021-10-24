bee2d5f20f65836092662b89c4a8d2eb12cf175b
soFs13 <n.sakaliyski@gmail.com>
2021-10-24 20:45:23 +0300

    Fix build warnings

--- src/simple-components/cell.js
+++ src/simple-components/cell.js
@@ -1,13 +1,9 @@
 import React from 'react';
 
-export default ({
-	rowIndex,
-	cellIndex,
-	value,
-	className,
-	onClick,
-}) => (
+const Cell = ({ rowIndex, cellIndex, value, className, onClick }) => (
 	<td className={className || undefined} onClick={onClick ? () => onClick(rowIndex, cellIndex) : undefined}>
 		{value}
 	</td>
 );
+
+export default Cell;
--- src/simple-components/player.js
+++ src/simple-components/player.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
-export default ({ data: { sign, name, color } }) => (
+const Player = ({ data: { sign, name, color } }) => (
 	<span style={{ fontSize: 20 }}>
 		(<span style={{color}}>{sign}</span>)
 		{name}
 	</span>
 );
+
+export default Player;
